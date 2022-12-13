04d3cbc71a4854ce646b7ad75944a544c94129b5
RayR124 <RealtorRayR@Gmail.com>
2022-12-12 17:59:46 -0800

    Started over from scratch basically. This is V2

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,39 @@
-const searchButton = document.getElementById("searchInput")
-const apiKey = //insert API key here
-const geoUrl = 'https://api.openweathermap.org/geo/1.0/direct?q='
-const currentUrl = 'https://api.openweathermap.org/data/2.5/weather?';
+const searchButton = document.getElementById(".searchBar");
+const apiKey = "68720197936e62b9527492c3bc6e930e";
+//const inputVal = input.value;
+const geoUrl = "https://api.openweathermap.org/geo/1.0/direct?q=";
+const currentUrl = "https://api.openweathermap.org/data/2.5/weather?";
+const url = "https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric";
 
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0');
-var yyyy = today.getFullYear();
-today = mm + '/' + dd + '/' + yyyy;
-currentDate.innerHTML = '(' + today + ')';
+let weather = {
+  fetchWeather: function (city) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric")
+      .then((response) => response.json()).then((data) => this.displayWeather(data));
+  },
 
-var date2 = new Date();
-var dd2 = String(date2.getDate() + 1).padStart(2, '0');
-date2 = mm + '/' + dd2 + '/' + yyyy;
-secondDay.innerHTML = date2
+  displayWeather: function (data) {
+    const { name } = data;
+    const { description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
 
-var date3 = new Date();
-var dd3 = String(date3.getDate() + 2).padStart(2, '0');
-date3 = mm + '/' + dd3 + '/' + yyyy;
-thirdDay.innerHTML = date3
+    document.querySelector(".cityName").innerText = "Currently in " + name;
+    document.querySelector(".description").innterText = description;
+    document.querySelector(".temp").innerText = temp + "°";
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind Speed: " + speed + "MPH";
+  },
 
-var date4 = new Date();
-var dd4 = String(date4.getDate() + 3).padStart(2, '0');
-date4 = mm + '/' + dd4 + '/' + yyyy;
-fourthDay.innerHTML = date4
+    search: function () {
+      this.fetchWeather(document.querySelector(".searchBar").value);
+    },
+  };
 
-var date5 = new Date();
-var dd5 = String(date5.getDate() + 4).padStart(2, '0');
-date5 = mm + '/' + dd5 + '/' + yyyy;
-fifthDay.innerHTML = date5
-
-var date6 = new Date();
-var dd6 = String(date6.getDate() + 5).padStart(2, '0');
-date6 = mm + '/' + dd6 + '/' + yyyy;
-sixthDay.innerHTML = date6
+    document.querySelector(".mainBtn").addEventListener("Click", function () {
+      weather.search();
+    });
+    document.querySelector(".searchBar").addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    })
