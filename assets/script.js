2441363e0da24e74751b541300d5f8527371d866
RayR124 <RealtorRayR@Gmail.com>
2022-12-15 14:08:45 -0800

    more lateral changes, no net positive.

--- assets/script.js
+++ assets/script.js
@@ -23,45 +23,48 @@ let weather = {
     document.body.style.backgroundImage = "url(\"https://source.unsplash.com/1600x900/?" + description + "\")";
 
     let forecast = {
-      fetchForecast: function (city) {
+      fetchForecast: function (data) {
         let lat = data.coord.lat;
         let lon = data.coord.lon;
         fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial").then((response) => response.json()).then((data) => this.displayForecast(data));
-        
+
         function displayForecast(data) {
           for (let i = 0; i < 5; i++) {
-            if (data.list[i].dt_txt.slice(11, 13) == "12:");
+            const { temp2 } = data.list[i].main.temp;
+            const { humidity2 } = data.list[i].main.humidity;
+            const { wind2 } = data.wind.speed;
+            document.querySelector(".day2Temp").innerHTML = temp2 + "°F";
+            document.querySelector(".day2Humidity").innerHTML = "Humidity: " + humidity2 + "%";
+            document.querySelector(".day2Wind").innerHTML = "Wind Speed: " + wind2 + "MPH";
+          } for (let i = 0; i < 5; i++) {
+            const { temp3 } = data.list[i].main.temp;
+            const { humidity3 } = data.list[i].main.humidity;
+            const { wind3 } = data.wind.speed;
+            document.querySelector(".day3Temp").innerHTML = temp3 + "°F";
+            document.querySelector(".day3Humidity").innerHTML = "Humidity: " + humidity3 + "%";
+            document.querySelector(".day3Wind").innerHTML = "Wind Speed: " + wind3 + "MPH";
+          } for (let i = 0; i < 5; i++) {
+            const { temp4 } = data.list[i].main.temp;
+            const { humidity4 } = data.list[i].main.humidity;
+            const { wind4 } = data.wind.speed;
+            document.querySelector(".day4Temp").innerHTML = temp4 + "°F";
+            document.querySelector(".day4Humidity").innerHTML = "Humidity: " + humidity4 + "%";
+            document.querySelector(".day4Wind").innerHTML = "Wind Speed: " + wind4 + "MPH";
+          } for (let i = 0; i < 5; i++) {
+            const { temp5 } = data.list[i].main.temp;
+            const { humidity5 } = data.list[i].main.humidity;
+            const { wind5 } = data.wind.speed;
+            document.querySelector(".day5Temp").innerHTML = temp5 + "°F";
+            document.querySelector(".day5Humidity").innerHTML = "Humidity: " + humidity5 + "%";
+            document.querySelector(".day5Wind").innerHTML = "Wind Speed: " + wind5 + "MPH";
+          } for (let i = 0; i < 5; i++) {
+            const { temp6 } = data.list[i].main.temp;
+            const { humidity6 } = data.list[i].main.humidity;
+            const { wind6 } = data.wind.speed;
+            document.querySelector(".day6Temp").innerHTML = temp6 + "°F";
+            document.querySelector(".day6Humidity").innerHTML = "Humidity: " + humidity6 + "%";
+            document.querySelector(".day6Wind").innerHTML = "Wind Speed: " + wind6 + "MPH";
           }
-          const { temp2 } = data;
-          const { humidity2 } = data.main;
-          const { wind2 } = data.wind;
-          document.querySelector(".day2Temp").innerText = temp2 + "°F";
-          document.querySelector(".day2Humidity").innerText = "Humidity: " + humidity2 + "%";
-          document.querySelector(".day2Wind").innerText = "Wind Speed: " + wind2 + "MPH";
-          const { temp3 } = data;
-          const { humidity3 } = data.main;
-          const { wind3 } = data.wind;
-          document.querySelector(".day3Temp").innerText = temp3 + "°F";
-          document.querySelector(".day3Humidity").innerText = "Humidity: " + humidity3 + "%";
-          document.querySelector(".day3Wind").innerText = "Wind Speed: " + wind3 + "MPH";
-          const { temp4 } = data;
-          const { humidity4 } = data.main;
-          const { wind4 } = data.wind;
-          document.querySelector(".day4Temp").innerText = temp4 + "°F";
-          document.querySelector(".day4Humidity").innerText = "Humidity: " + humidity4 + "%";
-          document.querySelector(".day4Wind").innerText = "Wind Speed: " + wind4 + "MPH";
-          const { temp5 } = data;
-          const { humidity5 } = data.main;
-          const { wind5 } = data.wind;
-          document.querySelector(".day5Temp").innerText = temp5 + "°F";
-          document.querySelector(".day5Humidity").innerText = "Humidity: " + humidity5 + "%";
-          document.querySelector(".day5Wind").innerText = "Wind Speed: " + wind5 + "MPH";
-          const { temp6 } = data;
-          const { humidity6 } = data.main;
-          const { wind6 } = data.wind;
-          document.querySelector(".day6Temp").innerText = temp6 + "°F";
-          document.querySelector(".day6Humidity").innerText = "Humidity: " + humidity6 + "%";
-          document.querySelector(".day6Wind").innerText = "Wind Speed: " + wind6 + "MPH";
         } displayForecast();
       }
     }
